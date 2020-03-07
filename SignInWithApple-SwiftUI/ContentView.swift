beac3183a35b42ae1cdbdbc826aaaf3cacd2e546
Heiya <morooka.akira.inon29@gmail.com>
2020-03-07 15:05:36 +0900

    Add Project files.

---
+++ SignInWithApple-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SignInWithApple-SwiftUI
+//
+//  Created by akira morooka on 2020/03/07.
+//  Copyright © 2020 akira morooka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
